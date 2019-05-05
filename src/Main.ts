2389266cfeffd639cc1d9dd24aa917455a26f60b
mon6912640 <010monkey@163.com>
2019-05-05 21:07:32 +0800

    测试压缩配置代码

--- src/Main.ts
+++ src/Main.ts
@@ -34,13 +34,60 @@ class Main extends egret.DisplayObjectContainer {
         FguiMgr.ins().addPackage("main");
         this.addChild(fairygui.GRoot.inst.displayObject);
 
-        let t_mainUI = new MainUIView();
-        fairygui.GRoot.inst.addChild(t_mainUI.view);
-        t_mainUI.start();
+        // let t_mainUI = new MainUIView();
+        // fairygui.GRoot.inst.addChild(t_mainUI.view);
+        // t_mainUI.start();
 
         // await platform.login();
         // const userInfo = await platform.getUserInfo();
         // console.log(userInfo);
+
+        // let t_num1 = 0b1;
+        // let t_num2 = 0b10;
+        // let t_num3 = 0b100;
+
+        // let t_result = 0;
+        // t_result |= t_num1;
+        // t_result |= t_num1;
+        // console.log(t_result.toString(2));
+        // console.log((t_result&t_num1) == t_num1);
+        // console.log((t_result&t_num2) == t_num2);
+        // console.log((t_result&t_num3) == t_num3);
+
+        // {
+        //     let t_buffs = RES.getRes("config_zip");
+        //     //  console.log(t_buffs);
+        //     let t_zlib = new Zlib.RawInflate(t_buffs);
+        //     let t_byte = new egret.ByteArray(t_zlib.decompress());
+        //     let t_you = t_byte.readUTFBytes(t_byte.length);
+        //     //  console.log(t_you);
+        //     let t_jsonStr = JSON.parse(t_you);
+        //     console.log("ok");
+        // }
+
+        // {
+        //     let t_buffs = RES.getRes("0config_zlib64");
+        //     //  console.log(t_buffs);
+        //     let t_zlib = new Zlib.RawInflate(t_buffs);
+        //     let t_byte = new egret.ByteArray(t_zlib.decompress());
+        //     let t_you = t_byte.readUTFBytes(t_byte.length);
+        //     //  console.log(t_you);
+        //     let t_str = atob(t_you)
+        //     let t_jsonStr = JSON.parse(t_str);
+        //     console.log("ok");
+        // }
+
+        {
+            let t_buffs = RES.getRes("0config_zlib");
+            //  console.log(t_buffs);
+            let t_zlib = new Zlib.RawInflate(t_buffs);
+            let t_byte = new egret.ByteArray(t_zlib.decompress());
+            let t_you = t_byte.readUTFBytes(t_byte.length);
+            //  console.log(t_you);
+            let t_jsonStr = JSON.parse(t_you);
+            console.log("ok");
+        }
+
     }
 
     private async loadResource() {
